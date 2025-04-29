172d33f0492d3c1d0c578c9f24edd89dddc33b6e
roninanastasiia <soul_bler@mail.ru>
2025-04-30 00:38:38 +0300

    изменение наименования поля ПСК в таблице

--- Data/Models/Contract.cs
+++ Data/Models/Contract.cs
@@ -27,10 +27,10 @@ public class Contract
     public string ContractStatus { get; set; }
     public Guid? CreditIssuanceAccount { get; set; }
     public Guid? DebitFromAccount { get; set; }
-    public decimal? MonthlyPaymentAmount { get; set; }
-    public decimal? TotalPaymentAmount { get; set; }
-    public decimal? TotalInterestPaid { get; set; }
-    public decimal? Psk { get; set; }
+    public decimal? MonthlyPaymentAmount { get; set; } // Сумма основного долга
+    public decimal? TotalPaymentAmount { get; set; } // Общая сумма денежных средств, которую заемщик выплатит кредитору за весь срок кредита.
+    public decimal? TotalInterestPaid { get; set; } // Все проценты, начисленные за весь срок кредита 
+    public decimal? FullLoanValue { get; set; } // ПСК - полная стоимость кредита. Это процентная ставка, выраженная в годовых процентах, которая отражает все расходы заёмщика, связанные с получением и обслуживанием кредита.
     public Guid? PaymentScheduleId { get; set; }
     public string AdditionalTerms { get; set; }
     public DateTime LastModificationDate { get; set; }
